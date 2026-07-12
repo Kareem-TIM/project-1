e63efc0206adca010b852f708d45f933e5357be5
Abdo <abdalrahman9803@gmail.com>
2026-07-12 22:34:07 +0300

    fix: update fetchStudents API endpoint to use a static URL

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ let id = 0;
 
 async function fetchStudents() {
     try {
-        const response = await fetch(`${{ vars.APILINK }}`);
+        const response = await fetch("https://studens-api-pyfastapi-beige.vercel.app/students");
 
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
